a52c8662d60d3d0d72ca9121bcb8da9f2314364b
cairq <cairq@tongbaninfo.com>
2023-06-06 20:53:45 +0800

    20230606

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: cairq cairq@tongbaninfo.com
+ * @Date: 2023-06-01 22:38:40
+ * @LastEditors: cairq cairq@tongbaninfo.com
+ * @LastEditTime: 2023-06-06 20:53:36
+ * @FilePath: \dateof_money\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const getUserSend = require('./lib/getUserSend');
 // const parseByAi = require('./lib/parseByAI');
 const dotenv = require('dotenv');
@@ -22,6 +30,7 @@ async function init() {
         .then(({ fileStatus, resolve }) => {
           resolve.push(obj);
           writeFileHandle(filePath, JSON.stringify(resolve));
+          console.log(JSON.stringify(resolve))
         })
         .catch((err) => {
           writeFileHandle(filePath, JSON.stringify([obj]));
